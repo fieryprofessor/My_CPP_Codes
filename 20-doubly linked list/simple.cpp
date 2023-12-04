4166e152ea344cf57b7bed2756ba1e6971d69f1f
Om Singh <ninjaplays792020@gmail.com>
2023-12-04 12:37:18 +0530

    modified simple doubly linked list

--- 20-doubly linked list/simple.cpp
+++ 20-doubly linked list/simple.cpp
@@ -23,15 +23,71 @@ Doublylinkedlist(){
     head=NULL;
     tail=NULL;
 }
+
+void insertAtStart(int data){
+Node* new_node = new Node(data);
+ if(head==NULL){
+    head=new_node;
+    tail=new_node;
+    return;
+ }
+ new_node->next=head;
+ head->prev=new_node;
+ head=new_node;
+ return;
+}
+
+void insertAtLast(int data){
+    Node*new_node=new Node(data);
+    if(head==NULL){
+     head=new_node;
+     tail=new_node;
+     return;
+    }
+    tail->next=new_node;
+    new_node->prev=tail;
+    tail=new_node;
+    return;
+}
+
+void insertAtArbitraryPosition(int data,int k){
+    Node*new_node=new Node(data);
+    Node*temp=head;
+    int count=k-1;
+    while(count--){
+        temp=temp->next;
+    }
+    new_node->next=temp;
+    new_node->prev=temp->prev;
+    temp->prev->next=new_node;
+    temp->prev=new_node;
+    return;
+}
+
+void display(){
+    Node*temp=head;
+    while(temp!=NULL){
+        cout<<temp->val<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
+}
+
 };
 
 
 
 int main(){
-Node*new_node=new Node(3);
+
 Doublylinkedlist dll;
-dll.head=new_node;
-dll.tail=new_node;
-cout<<dll.head->val;
+dll.insertAtLast(1);
+dll.display();
+dll.insertAtLast(2);
+dll.display();
+dll.insertAtLast(4);
+dll.display();
+dll.insertAtArbitraryPosition(3,3);
+dll.display();
+
     return 0;
 }
