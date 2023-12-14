8ffdb30582ad9e11748ce3a9ffef684364f11ab2
Om Singh <ninjaplays792020@gmail.com>
2023-12-14 11:42:13 +0530

    stacks implementation using stl

---
+++ 22-stacks/simple3.cpp
@@ -0,0 +1,18 @@
+//stacks using STL
+#include<iostream>
+#include<stack>
+using namespace std;
+int main()
+{
+stack<int> st;
+st.push(1);
+st.push(2);
+st.push(3);
+st.push(4);
+st.push(5);
+cout<<st.top()<<endl;
+st.pop();
+st.pop();
+cout<<st.top()<<endl;
+    return 0;
+}
